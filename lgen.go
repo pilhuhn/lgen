b305d40e1014f6293954fdb5ac68174d706e50d2
Heiko W. Rupp <hwr@pilhuhn.de>
2017-01-31 15:52:32 +0100

    Add a metric with 2 different labels.

--- lgen.go
+++ lgen.go
@@ -12,11 +12,17 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	rpg := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	fmt.Fprintf(w, "# TYPE metric_without_timestamp_and_labels gauge\n")
+	fmt.Fprintf(w, "%s\n", "# TYPE metric_without_timestamp_and_labels gauge")
 	fmt.Fprintf(w, "metric_without_timestamp_and_labels %3.1f\n", rpg.Float32() * 100)
 
-	fmt.Fprintf(w, "# TYPE metric_without_timestamp_and_labels2 gauge\n")
+	fmt.Fprintf(w, "%s\n","# TYPE metric_without_timestamp_and_labels2 gauge")
 	fmt.Fprintf(w, "metric_without_timestamp_and_labels2 %3.1f\n", rpg.Float32() * 42)
+
+	fmt.Fprintf(w, "%s\n","# TYPE metric_with_label counter")
+	fmt.Fprintf(w, "metric_with_label{code=\"200\"} %3.0f\n", rpg.Float32() * 42)
+	fmt.Fprintf(w, "metric_with_label{code=\"500\"} %3.0f\n", rpg.Float32() * 42)
+
+
 
 	time.Sleep( time.Duration(rpg.Float32() * 40 )* time.Millisecond)
 }
